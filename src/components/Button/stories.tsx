392668409fd6e686b0073e5931711fdc644898ed
Rodrigo Alves <rodrigoalvesbrasileiro@gmail.com>
2021-09-15 21:23:36 -0300

    feat: new component button

---
+++ src/components/Button/stories.tsx
@@ -0,0 +1,19 @@
+import { Meta, Story } from "@storybook/react";
+
+import Button from ".";
+
+export default {
+  title: "Button",
+  component: Button,
+  argTypes: {
+    children: {
+      type: "string"
+    }
+  }
+} as Meta;
+
+export const Default: Story = (args) => <Button {...args} />;
+
+Default.args = {
+  children: "Buy now"
+};
