f52641535cc41caf824387d1987d10a5dea7de0a
Rodrigo Alves <rodrigoalvesbrasileiro@gmail.com>
2021-09-15 21:42:12 -0300

    feat: adjustments in button and tests

--- src/components/Button/stories.tsx
+++ src/components/Button/stories.tsx
@@ -2,12 +2,17 @@ import { Meta, Story } from "@storybook/react";
 
 import Button from ".";
 
+import { AddShoppingCart } from "@styled-icons/material-outlined/AddShoppingCart";
+
 export default {
   title: "Button",
   component: Button,
   argTypes: {
     children: {
       type: "string"
+    },
+    icon: {
+      type: ""
     }
   }
 } as Meta;
@@ -16,4 +21,12 @@ export const Default: Story = (args) => <Button {...args} />;
 
 Default.args = {
   children: "Buy now"
+};
+
+export const withIcon: Story = (args) => <Button {...args} />;
+
+withIcon.args = {
+  size: "small",
+  children: "Buy now",
+  icon: <AddShoppingCart />
 };
