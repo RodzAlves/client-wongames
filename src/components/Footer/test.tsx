936f3045984c5760497ea9add302e6015e4bda30
Rodrigo Alves <rodrigoalvesbrasileiro@gmail.com>
2021-09-20 17:45:12 -0300

    feat: adjustements in button and footer

--- src/components/Footer/test.tsx
+++ src/components/Footer/test.tsx
@@ -1,11 +1,11 @@
-import { render, screen } from "@testing-library/react";
+import { screen } from "@testing-library/react";
 import { renderWithTheme } from "utils/tests/helpers";
-
 import Footer from ".";
 
 describe("<Footer />", () => {
   it("should render 4 columns topics", () => {
-    renderWithTheme(<Footer />);
+    const { container } = renderWithTheme(<Footer />);
+
     expect(
       screen.getByRole("heading", { name: /contact/i })
     ).toBeInTheDocument();
@@ -16,6 +16,8 @@ describe("<Footer />", () => {
     expect(
       screen.getByRole("heading", { name: /location/i })
     ).toBeInTheDocument();
+
+    expect(container.firstChild).toMatchSnapshot();
   });
 
   it("should render copyright", () => {
